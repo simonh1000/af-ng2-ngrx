893388b155a593636fbfd6945015fec97f0e7f08
Simon Hampton <hotbelgo@gmail.com>
2016-12-12 15:46:12 +0100

    Favourites - beta4

--- src/app/reducers/notices_reducer.ts
+++ src/app/reducers/notices_reducer.ts
@@ -10,14 +10,14 @@ const initialNotices: Notices = {
 export const INITIAL_NOTICES = 'INITIAL_NOTICES';
 export const DISMISS_FAVOURITES = 'DISMISS_FAVOURITES';
 
-export function noticesReducer(state: Object = initialNotices, action: Action) {
+export function noticesReducer(state: Notices = initialNotices, action: Action) {
   console.log('noticesReducer', action);
   switch (action.type) {
     case INITIAL_NOTICES:
       return Object.assign({}, state, action.payload)
 
     case DISMISS_FAVOURITES:
-      return Object.assign({}, state, {favouritesOverlayDismissed: true})
+      return Object.assign({}, state, {favouritesOverlayDismissed: !state.favouritesOverlayDismissed})
 
     default:
       return state;
